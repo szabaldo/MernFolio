dc4b51a41f1b0654cad83763bd4767990248d855
Sam Zabaldo <sam.zabaldo@gmail.com>
2024-10-03 13:29:08 -0400

    added confirmation with animation after comment submission

--- client/src/Home.js
+++ client/src/Home.js
@@ -14,14 +14,14 @@ function Home() {
     const headerOpts = user ? { subtitle: "", linkText: "", linkTo: "" } : { subtitle: "You are not logged in", linkText: "Log in", linkTo: "/login" }
 
     const submitComment = async () => {
-        // const response = await fetch(process.env.ORIGIN + "/comment", {
-        //     body: JSON.stringify({ comment: document.getElementById("commentField").value }),
-        //     method: "POST",
-        //     headers: { "Content-Type": "application/json" },
-        //     credentials: "include"
-        // });
-        // const res = await response.json();
-        // console.log(res);
+        const response = await fetch(process.env.ORIGIN + "/comment", {
+            body: JSON.stringify({ comment: document.getElementById("commentField").value }),
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            credentials: "include"
+        });
+        const res = await response.json();
+        console.log(res);
         document.getElementById("commentField").value = "";
         setComment("");
         setCommentSubmitted(true);
@@ -32,14 +32,21 @@ function Home() {
     }
 
     const handleCloseConfirm = () => {
-        let el = document.getElementById("commentConfirm");
-        el.classList.remove("rise-fade-middle");
-        el.offsetHeight;
-        el.classList.add("rise-fade-middle-out");
+        let confirmBox = document.getElementById("commentConfirm");
+        confirmBox.classList.remove("rise-fade-middle");
+        confirmBox.offsetHeight;
+        confirmBox.classList.add("rise-fade-middle-out");
+        let darkBackground = document.getElementById("darkBackground");
+        darkBackground.classList.remove("fade-in-50");
+        darkBackground.offsetHeight;
+        darkBackground.classList.add("fade-out-50");
         setTimeout(() => {
-            el.classList.remove("rise-fade-middle-out");
-            el.offsetHeight;
-            el.classList.add("rise-fade-middle");
+            confirmBox.classList.remove("rise-fade-middle-out");
+            confirmBox.offsetHeight;
+            confirmBox.classList.add("rise-fade-middle");
+            darkBackground.classList.remove("fade-out-50");
+            darkBackground.offsetHeight;
+            darkBackground.classList.add("fade-in-50");
             setCommentSubmitted(false);
         }, 1000);
     }
@@ -47,19 +54,22 @@ function Home() {
     const commentBox = (
         <div className={`${isIntro.current ? "rise-fade-dvs" : ""}`} id="commentBox">
             {commentSubmitted && (
-                <div className="rise-fade-middle position-fixed p-3 mx-auto rounded border shadow inner-width bg-white r-0 l-0" id="commentConfirm">
-                    <Container>
-                        <Row className="py-1">
-                            <h2 className="large-text d-flex justify-content-center">Thanks!</h2>
-                            <br />
-                            <p className="medium-text d-flex justify-content-center">Your comment is under review and will appear on the website once approved.</p>
-                        </Row>
-                        <Row className="py-1">
-                            <Col className="d-flex justify-content-center">
-                                <Button onClick={handleCloseConfirm}>Close</Button>
-                            </Col>
-                        </Row>
-                    </Container>
+                <div>
+                    <div className="fade-in-50" style={{ position: "absolute", width: "100vw", height: "100vh", backgroundColor: "black", top: 0, left: 0, zIndex: 1 }} id="darkBackground" />
+                    <div className="rise-fade-middle position-fixed p-3 mx-auto rounded border shadow inner-width bg-white r-0 l-0" id="commentConfirm" style={{ zIndex: 2, filter: "blur(-20px)" }}>
+                        <Container>
+                            <Row className="py-1">
+                                <h2 className="large-text d-flex justify-content-center">Thanks!</h2>
+                                <br />
+                                <p className="medium-text d-flex justify-content-center">Your comment is under review and will appear on the website once approved.</p>
+                            </Row>
+                            <Row className="py-1">
+                                <Col className="d-flex justify-content-center">
+                                    <Button onClick={handleCloseConfirm}>Close</Button>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </div>
                 </div>
             )}
             <Container>
